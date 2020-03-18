d4a371c227647b396d767d0a17a70a0c78df9cc5
karanb007 <karan1688.cse18@chitkara.edu.in>
2020-03-18 22:58:22 +0530

    made navbar functional and added back to top

---
+++ nav_scroll.js
@@ -0,0 +1,43 @@
+  $(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 1 });   
+  
+    $("#myNavbar a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 400, function(){
+     
+          window.location.hash = hash;
+        });
+      }
+    });
+    $("#back-to-top").on('click', function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+    
+          var hash = this.hash;
+    
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 300, function(){
+       
+            window.location.hash = hash;
+          });
+        } 
+      });
+
+    var amountScrolled = 2500;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('#back-to-top').fadeIn(300);
+	} else {
+		$('#back-to-top').fadeOut(300);
+	}
+});
+  });
